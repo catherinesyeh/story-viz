dbd4134e3eb0dc1bc86e5629df91f0f5509eb471
Catherine Yeh <catherineyeh@g.harvard.edu>
2024-04-13 21:26:56 -0400

    adding book/movie image

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,34 @@ function App() {
   return (
     <div id="app">
       <header>
-        <div id="story-info">
-          <h1>{data["title"]}</h1>
-          <span>
-            {data["author"] ? data["author"] : data["director"]}{" "}
-            <Divider orientation="vertical" /> {data["year"]}{" "}
-            <Divider orientation="vertical" />{" "}
-            <a href={data["url"]} target="_blank" title={data["title"]}>
-              <Button
-                size="xs compact"
-                variant="light"
-                id="info-button"
-                leftSection={<FiFileText />}
-              >
-                Full {data["type"] === "Movie" ? "Script" : "Text"}
-              </Button>
-            </a>
-          </span>
+        <div id="story-header">
+          <a href={data["url"]} target="_blank" title={data["title"]}>
+            <img
+              src={data["image"]}
+              alt={data["title"]}
+              className="story-image"
+            />
+          </a>
+          <div id="story-info">
+            <h1>{data["title"]}</h1>
+            <span>
+              {data["author"] ? data["author"] : data["director"]}{" "}
+              <Divider orientation="vertical" /> {data["year"]}{" "}
+              <Divider orientation="vertical" />{" "}
+              <a href={data["url"]} target="_blank" title={data["title"]}>
+                <Button
+                  size="xs compact"
+                  variant="light"
+                  id="info-button"
+                  leftSection={<FiFileText />}
+                >
+                  Full {data["type"] === "Movie" ? "Script" : "Text"}
+                </Button>
+              </a>
+            </span>
+          </div>
         </div>
+
         <PlotOptions />
       </header>
       <div id="story-contain">
